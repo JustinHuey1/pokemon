afd0a7265f381962815f74cf2ce5216150a255cb
Alex Huynh <alexh3925@gmail.com>
2019-04-24 01:16:24 +0000

    HTML and Javascript

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@
 var randPoke = Math.ceil(Math.random * 807);
 var botUrl = `https://pokeapi.co/api/v2/pokemon/${randPoke}`;
 
+var pokemon1 = false;
+var pokemon2 = false;
+var pokemon3 = false;
+var pokemon4 = false;
+var pokemon5 = false;
+var pokemon6 = false;
 //page linking
 // Start Page//
 $("#start").click(function(){
@@ -16,35 +22,51 @@ $("#rule").click(function(){
 
 });
 
+$("#allRules").hide();
+$("#rule").click(function(){
+    $("#allRules").slideToggle();
+});
 // Team Page//
+
 $("#findPokemon").click(function(){
-    $(".a").hide();
-    // var searchTerm = $("#searchTerm").val();
-    // var apiUrl = `https://pokeapi.co/api/v2/pokemon/${searchTerm}/`;
-    // $(".picture").html("");
-    // $.ajax({
-    //     url: apiUrl,
-    //     method: "GET",
-    //     success: function(response){
-    //         // $(".picture").append(`<img class = "pokeImg" src="${response.sprites.front_default}">`);
-    //         // $(".pokeAdd").append('<button id = "choose">Add Pokemon to team</button>');
-    //     }
-    // });
+    var searchTerm = $("#searchTerm").val();
+    var apiUrl = `https://pokeapi.co/api/v2/pokemon/${searchTerm}/`;
+    $(".picture").html("");
+    $("#ability").html("");
+    $.ajax({
+        url: apiUrl,
+        method: "GET",
+        success: function(response){
+            $(".name").append(`<p class = "`)
+            $(".picture").append(`<img class = "pokeImg" src="${response.sprites.front_default}">`);
+            response.abilities.forEach(function(x){
+                $("#ability").append(`<p class = "pokeAbility">${x.ability.name}</p>`);
+            });
+        }
+    });
 });
 
 $("#choose").click(function(){
-    
-}
-
-// Battle Page//
-$("#ability1").click(function(){
+    var searchTerm = $("#searchTerm").val();
+    var apiUrl = `https://pokeapi.co/api/v2/pokemon/${searchTerm}/`;
     $.ajax({
-        //url: apiUrl,
+        url: apiUrl,
         method: "GET",
-        success : function(response){
-            var randNum = Math.floor(Math.random * response.abilities[0].ability.length);
-            var ability = response.abilities[randNum].ability.name;
+        success: function(response){
+            $("#pokemon1").append(`<img src="${response.sprites.front_default}">`);
         }
-        
     });
 });
+
+// Battle Page//
+// $("#ability1").click(function(){
+//     $.ajax({
+//         //url: apiUrl,
+//         method: "GET",
+//         success : function(response){
+//             var randNum = Math.floor(Math.random * response.abilities[0].ability.length);
+//             var ability = response.abilities[randNum].ability.name;
+//         }
+        
+//     });
+// });
